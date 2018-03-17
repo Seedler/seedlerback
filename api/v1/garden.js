43e367ab4fd8b704a413e750639d53aaf79db1d9
Asondo <asondo.kun@gmail.com>
2018-03-17 12:21:01 +0300

    ---- api/garden fix ownerId and req.user usage

--- api/v1/garden.js
+++ api/v1/garden.js
@@ -1,41 +1,46 @@
 'use strict';
 
-const projectKeeper = require('../../libs/projectKeeper');
-const logger = projectKeeper.getLogger('api/v1/garden');
+// const projectKeeper = require('../../libs/projectKeeper');
+// const logger = projectKeeper.getLogger('api/v1/garden');
 const controller = require('../../controller');
 const {
     ACCESS_LEVELS = {},
     wrapMethod = func => func,
+    extractUserFromParams = func => func,
 } = controller;
 
 const Garden = require('../../models/garden');
 const Tenure = require('../../models/tenure');
 
 async function addGarden(params = {}) {
+    const {
+        id,
+    } = extractUserFromParams(params);
+
+    params.ownerId = id;
+
     const garden = await new Garden(params).insertIntoDB();
 
     return garden.safeData;
 }
 
 function getGardens(params = {}) {
-    const user = controller.extractUserFromParams(params);
     const {
         id: ownerId = 0,
-    } = user;
+    } = extractUserFromParams(params);
 
     return Garden.getManyFromDB({ownerId});
 }
 
 async function getAvailableList(params = {}) {
-    const user = controller.extractUserFromParams(params);
     const {
         id: keeperId = 0,
-    } = user;
+    } = extractUserFromParams(params);
 
     const tenures = await Tenure.getManyFromDB({keeperId});
     const gardenId = tenures.map(tenure => tenure.gardenId);
 
-    return Garden.getManyFromDB({gardenId});
+    return Garden.getManyFromDB({id: gardenId});
 }
 
 module.exports = {
